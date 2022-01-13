f44594bff79ae21cc3a05290b3f18671b162bd35
Dev-Elie <eochieng9448@gmail.com>
2022-01-13 11:58:27 +0300

    Modified routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var userRouter = require('./routes/user');
 
+
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/users/:id', userRouter);
 
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+function fetchData(){
+  var fs = require('fs');
+  var path = require('path');
+  
+  let rawdata = fs.readFileSync(path.resolve(__dirname, 'users-data.json'));
+  let users = JSON.parse(rawdata);
+
+  return users
+}
+
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send([
-    {
-      id: 1,
-      name: 'John Doe',
-      role: 'CTO',
-      age: 35,
-      twitter_url: 'https://twitter.com/dev_elie',
+  const users = fetchData();
 
-    },
-    {
-      id: 2,
-      name: 'Mike Spencer',
-      role: 'Web Developer',
-      age: 25,
-      twitter_url: 'https://twitter.com/dev_elie',
-
-    },
-    {
-      id: 3,
-      name: 'Ondiek Elijah',
-      role: 'Python Developer',
-      age: 21,
-      twitter_url: 'https://twitter.com/dev_elie',
-
-    }
-  ]);
+  res.send(
+    users
+  );
 });
 
 module.exports = router;
