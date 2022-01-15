b1695e874bc22677e553d87456ef395328954dd1
Dev-Elie <eochieng9448@gmail.com>
2022-01-15 19:29:14 +0300

    Updated Circle CI config.yml

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function (err, req, res, next) {
 });
 
 // Connect to db
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bfhjd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+mongoose.connect(process.env.DATABASE_URI)
   // mongoose.connect(process.env.DB_CONNECTION)
   .then(() => console.log("DB connected!"))
   .catch(err => console.log(err));
--- tests/testDBsetup.js
+++ tests/testDBsetup.js
@@ -6,10 +6,10 @@ const testPostData = { title: 'Before all, create posts', description: 'post con
 
 
 const connect = async () => {
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bfhjd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-        // mongoose.connect(process.env.DB_CONNECTION)
-        .then(() => console.log("DB connected!"))
-        .catch(err => console.log(err));
+    mongoose.connect(process.env.DATABASE_URI)
+    // mongoose.connect(process.env.DB_CONNECTION)
+    .then(() => console.log("DB connected!"))
+    .catch(err => console.log(err));
 
     const validPost = new PostModel(testPostData);
     await validPost.save();
